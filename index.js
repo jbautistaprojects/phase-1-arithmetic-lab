09a79a354d094644a7e98bb08cd7f21a65369e52
Justina Mandaro <bautistajustina7@gmail.com>
2022-07-26 13:27:00 -0700

    added code to pass tests

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+
+/*function multiply (num1, num2) {
+    return(num1 * num2);
+}
+multiply(num1, num2);*/
+
+const multiply = num1*num2;
+
+/*function random() {
+    num = (Math.random()*10);
+    return(Math.round(num));
+    
+}
+random();*/
+
+const randomNum = Math.random()*10;
+const random = Math.round(randomNum);
+console.log(random);
+
+const num3 = 22;
+const num4 = 6;
+const mod = 22%6;
+console.log(mod);
+
+const max = Math.max(1, 11, 15, 20);
+console.log(max);
